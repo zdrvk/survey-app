6a0049ec174639505630366d9fd394cf261eed07
Zdravko Koev <zdravko.j.koev@gmail.com>
2024-01-24 11:20:25 +0000

    Amend buttons and add a path to Sign out

--- src/Components/DefaultLayout.jsx
+++ src/Components/DefaultLayout.jsx
@@ -19,6 +19,11 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ');
 }
 
+const logout = (e) => {
+    e.preventDefault();
+    console.log('logout');
+};
+
 export default function DefaulLayout() {
     return (
         <>
@@ -84,23 +89,15 @@ export default function DefaulLayout() {
                                                     leaveTo='transform opacity-0 scale-95'
                                                 >
                                                     <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                                                        {userNavigation.map((item) => (
-                                                            <Menu.Item key={item.name}>
-                                                                {({ active }) => (
-                                                                    <a
-                                                                        href={item.href}
-                                                                        className={classNames(
-                                                                            active
-                                                                                ? 'bg-gray-100'
-                                                                                : '',
-                                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                                        )}
-                                                                    >
-                                                                        {item.name}
-                                                                    </a>
-                                                                )}
-                                                            </Menu.Item>
-                                                        ))}
+                                                        <Menu.Item>
+                                                            <a
+                                                                href='#'
+                                                                onClick={(e) => logout(e)}
+                                                                className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 '
+                                                            >
+                                                                Sign out
+                                                            </a>
+                                                        </Menu.Item>
                                                     </Menu.Items>
                                                 </Transition>
                                             </Menu>
@@ -165,16 +162,13 @@ export default function DefaulLayout() {
                                         </div>
                                     </div>
                                     <div className='mt-3 space-y-1 px-2'>
-                                        {userNavigation.map((item) => (
-                                            <Disclosure.Button
-                                                key={item.name}
-                                                as='a'
-                                                href={item.href}
-                                                className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
-                                            >
-                                                {item.name}
-                                            </Disclosure.Button>
-                                        ))}
+                                        <Disclosure.Button
+                                            onClick={(e) => logout(e)}
+                                            href='#'
+                                            className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
+                                        >
+                                            Sign out
+                                        </Disclosure.Button>
                                     </div>
                                 </div>
                             </Disclosure.Panel>
