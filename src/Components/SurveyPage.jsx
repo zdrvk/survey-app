c2e6e1e1c3018a20c6975bde865311c8d1beb421
Zdravko Koev <zdravko.j.koev@gmail.com>
2024-02-06 18:56:10 +0000

    Display create survey errors

--- src/Components/SurveyPage.jsx
+++ src/Components/SurveyPage.jsx
@@ -7,6 +7,7 @@ import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function SurveyPage() {
     const navigate = useNavigate();
+    const [error, setError] = useState('');
 
     const [survey, setSurvey] = useState({
         title: '',
@@ -44,8 +45,41 @@ export default function SurveyPage() {
         }
         delete payload.image_url;
 
-        axiosClient.post('/survey', payload).then((res) => {});
-        navigate('/surveys');
+        axiosClient
+            .post('/survey', payload)
+            .then((res) => {
+                console.log(res);
+                navigate('/surveys');
+            })
+            .catch((error) => {
+                setError(error.response.data);
+                console.log(error);
+            });
+        // axiosClient
+        //     .post('/survey', payload)
+        //     .then((res) => {
+        //         console.log(res);
+        //         navigate('/surveys');
+        //     })
+        //     .catch((error) => {
+        //         if (error.response && error.response.data && error.response.data.errors) {
+        //             const errorMessages = [];
+        //             const errors = error.response.data.errors;
+        //             for (const field in errors) {
+        //                 if (errors.hasOwnProperty(field)) {
+        //                     const fieldErrors = errors[field];
+        //                     fieldErrors.forEach((errorMessage) => {
+        //                         errorMessages.push(`${field}: ${errorMessage}`);
+        //                     });
+        //                 }
+        //             }
+        //             const errorMessage = errorMessages.join('<br><br>');
+        //             setError(errorMessage);
+        //         } else {
+        //             setError('An error occurred. Please try again later.');
+        //         }
+        //         console.log(error);
+        //     });
     };
 
     return (
@@ -99,7 +133,9 @@ export default function SurveyPage() {
                                 className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
                             />
                         </div>
-
+                        <small className='text-red-500'>
+                            {error?.errors?.title && error.errors.title}
+                        </small>
                         {/*Description*/}
                         <div className='col-span-6 sm:col-span-3'>
                             <label
@@ -140,6 +176,9 @@ export default function SurveyPage() {
                                 className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
                             />
                         </div>
+                        <small className='text-red-500 py-2'>
+                            {error?.errors?.expire_date && error.errors.expire_date}
+                        </small>
                         {/*Expire Date*/}
                         {/*Active*/}
                         <div className='flex items-start'>
@@ -164,9 +203,17 @@ export default function SurveyPage() {
                                 </p>
                             </div>
                         </div>
+
                         {/*Active*/}
-                        <div className=' px-4 py-3 text-right sm:px-6'>
-                            <TButton>Save</TButton>
+                        <div className=' px-4 py-3 text-right sm:px-6 flex flex-col sm:flex-row gap-6'>
+                            <div>
+                                <TButton>Save</TButton>
+                            </div>
+                            {error && (
+                                <div className='bg-red-500 text-white py-2 px-3 rounded-lg w-full text-left'>
+                                    {error.message}
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
