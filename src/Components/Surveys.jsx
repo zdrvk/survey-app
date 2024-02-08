22d144df4978b0f8a025ac7911e149be947c9352
Zdravko Koev <zdravko.j.koev@gmail.com>
2024-02-08 15:21:30 +0000

    Show created surveys

--- src/Components/Surveys.jsx
+++ src/Components/Surveys.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Page from './Page';
 import { useStateContext } from '../Contexts/ContextProvider';
 import SurveyListItem from './SurveyListItem';
 import { PlusCircleIcon } from '@heroicons/react/24/outline';
 import TButton from './TButton';
+import axiosClient from '../axios';
 
 export default function Surveys() {
-    const { surveys } = useStateContext();
+    // const { surveys } = useStateContext();
+    const [surveys, setSurveys] = useState([]);
+
     console.log(surveys);
 
     const onDeleteClick = () => {
         console.log('On Delete click');
     };
+
+    useEffect(() => {
+        axiosClient.get('/survey').then(({ data }) => {
+            setSurveys(data.data);
+        });
+    }, []);
+
     return (
         <Page
             title='Surveys'
